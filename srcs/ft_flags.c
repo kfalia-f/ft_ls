ed7f870c5ca3c686a1804333cd2060df284d65aa
Kassandra Falia flowers <kfalia-f@e2r2p5.21-school.ru>
2019-03-28 15:53:27 +0300

    flags

--- srcs/ft_flags.c
+++ srcs/ft_flags.c
@@ -1,44 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_flags.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfalia-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/28 15:03:17 by kfalia-f          #+#    #+#             */
+/*   Updated: 2019/03/28 15:45:08 by kfalia-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <ft_ls.h>
 
-static void    ft_error(char c)
+static void    ft_wrong_flag(char c)
 {
-    // print usage / perror and exit.
+	ft_putstr("ft_ls: illegal option -- ");
+	ft_putchar(c);
+	ft_putendl("\nusage: ./ft_ls [-Rafltr1] [file ...]", 0);
+	exit(EXIT_FAILURE);
 }
 
-static void    ft_check_flag(char *s, t_flags flags)
+static void    ft_check_flag(char *str, t_flags flags)
 {
     size_t  i;
-    size_t  j;
+	size_t	bits_to_shift;
     char    *cmp;
+	char	*s;
     
     i = 1;
-    j = 0;
     cmp = "Raflrt1";
-    while (s[i] != '\0')
+    while (str[i])
     {
-        while (cmp[j] != '\0')
-        {
-            if (s[i] == cmp[j])
-            {
-                //do the job with bits
-            }
-            else
-                ft_error(s[i]);
-            j++;
-        }
-        i++;
+		if ((s = ft_strchr(cmp, str[i])) != NULL)
+			bits_to_shift = s - cmp;
+		else
+			ft_wrong_flag(str[i]);
+		flags.value |= (1 << bits_to_shift);
+		i++;
     }
 }
 
 size_t        ft_flags(int ac, char **av, t_flags flags)
 {
-    size_t  i;
+    int  i;
     size_t  num_of_flags;
 
     i = 1;
     num_of_flags = 0;
     while (i < ac)
     {
+        if (av[i][0] == '-' && av[i][1] == '-')
+        {
+            num_of_flags++;
+			if (av[i][2] != '\0' && av[i][2] != ' ' && av[i][2] != '\t' && av[i][2] != '\n')
+				ft_wrong_flag('-');
+            break ;
+        }
         if (av[i][0] == '-')
         {
             ft_check_flag(av[i], flags);
@@ -46,11 +63,6 @@ size_t        ft_flags(int ac, char **av, t_flags flags)
         }
         else
             break ;
-        if (av[i][0] == '-' && av[i][1] == '-')
-        {
-            num_of_flags++;
-            break ;
-        }
         i++;
     }
     return (num_of_flags);
