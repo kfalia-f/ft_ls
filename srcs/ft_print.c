b93ccd4e271c537dca659a015846791e8bcfe2d4
Korbin Parker <koparker@e2r2p4.21-school.ru>
2019-04-02 21:32:11 +0300

    ft_print done. Clean up of ft_print nedded

--- srcs/ft_print.c
+++ srcs/ft_print.c
@@ -6,7 +6,7 @@
 /*   By: koparker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 14:08:44 by koparker          #+#    #+#             */
-/*   Updated: 2019/04/02 20:52:33 by koparker         ###   ########.fr       */
+/*   Updated: 2019/04/02 21:30:53 by koparker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_print_contents(char **names, size_t max_len, size_t col)
 {
+	struct winsize w;
 	size_t	row;
 	size_t	num_of_elems;
 	size_t	j;
@@ -24,6 +25,7 @@ void	ft_print_contents(char **names, size_t max_len, size_t col)
 		while ((*tmp->name) == '.')
 			tmp = tmp->next;
 	}*/
+	ioctl(0, TIOCGWINSZ, &w);
 	num_of_elems = ft_2d_strlen(names);
 	((row = num_of_elems / col) && (num_of_elems % col == 0)) ? row : (row = row + 1);
 //	printf("row %zu col %zu\n", row, col);
@@ -34,14 +36,20 @@ void	ft_print_contents(char **names, size_t max_len, size_t col)
 		while (i < num_of_elems)
 		{
 			ft_putstr(names[i]);
-			ft_output_spaces(' ', max_len - ft_strlen(names[i]) + 1);
+			if (col == 1 && ft_strlen(names[i]) <= w.ws_col)
+			{
+				ft_output_spaces(' ', w.ws_col - ft_strlen(names[i]));
+			}
+			else
+				ft_output_spaces(' ', max_len - ft_strlen(names[i]) + 1);
 			if (col == 1 && j + 1 == row)
 			{
 	//			printf("j + 1 == %zu\n", j + 1);
 				//ij++;
 				break ;
 			}
-			ft_putchar('\t');
+			if (col != 1)
+				ft_putchar('\t');
 			i += row;
 		}
 	//	else
