81b44f39c6eef523064356cc2894fa8007673c07
Kassandra Falia flowers <kfalia-f@oa-j5.21-school.ru>
2019-04-11 16:59:15 +0300

    fix data time

--- srcs/ft_l_flag.c
+++ srcs/ft_l_flag.c
@@ -6,7 +6,7 @@
 /*   By: kfalia-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 16:24:27 by kfalia-f          #+#    #+#             */
-/*   Updated: 2019/04/11 13:31:03 by kfalia-f         ###   ########.fr       */
+/*   Updated: 2019/04/11 16:58:56 by kfalia-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,27 @@ char	*get_permission(mode_t st_mode)
 	return (str);
 }
 
-char	*ft_date(char *date)
+char	*ft_date(char *date, size_t tm)
 {
 	char	*str;
 	int		i;
 	int		j;
+	int		flag;
 
 	i = 0;
 	j = 0;
+	flag = 0;
+	if (time(NULL) - tm > 15552000)
+		flag = 1;
 	str = ft_memalloc(13);
-	while (date[i])
+	while (date[i] != '\n')
 	{
 		if (i < 4)
+		{
+			i++;
+			continue ;
+		}
+		if (flag == 1 && i >= 11 && i <	19)
 		{
 			i++;
 			continue ;
@@ -89,7 +98,7 @@ void	get_info(char *file_name, t_lflag *st)
 	st->permissions = get_permission(buff.st_mode); //permissions (r/w/x) + file type
 	st->owner = ft_strcpy(ft_memalloc(ft_strlen(pwd->pw_name)), pwd->pw_name); //owner
 	st->group = ft_strcpy(ft_memalloc(ft_strlen(gr->gr_name)), gr->gr_name);  //group
-	st->date = ft_date(ctime(&buff.st_mtime));
+	st->date = ft_date(ctime(&buff.st_mtime), buff.st_mtime);
 }
 
 static int		ft_max_llen(t_lflag *st, int flag)  //1 = link; 2 = size; 3 = owner; 4 = group
@@ -147,10 +156,26 @@ void	ft_output_info(t_lflag *st)
 	}
 }
 
+int		ft_total(char *path_name, t_lflag *st)
+{
+	struct stat	buff;
+	t_lflag 	*tmp;
+	int			total;
+
+	tmp = st;
+	total = 0;
+	while (tmp)
+	{
+		stat(ft_str_path(path_name, tmp->file_name), &buff);
+		total += buff.st_blocks;
+		tmp = tmp->next;
+	}
+	return (total);
+}
+
 void	ft_l(char *path_name, t_flags flags)
 {
 	DIR				*dirp;
-	struct stat		buff;
 	struct dirent	*dp;
 	t_lflag			*lhead;
 	t_lflag			*lnode;
@@ -172,8 +197,9 @@ void	ft_l(char *path_name, t_flags flags)
 		get_info(ft_str_path(path_name, lnode->file_name), lnode);
 		lnode = lnode->next;
 	}
-	stat(path_name, &buff);
-	printf ("total %lld\n", buff.st_blocks);
+	ft_putstr("total ");
+	ft_putnbr(ft_total(path_name, lhead));
+	ft_putchar('\n');
 	ft_output_info(lhead);
 	//ft_free_list(lhead);
 }
