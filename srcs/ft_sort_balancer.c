3b775adcf28aca525aa65503192f87c9f8d22345
Kassandra Falia flowers <kfalia-f@vo-k7.21-school.ru>
2019-04-24 19:16:45 +0300

    merge fix

--- srcs/ft_sort_balancer.c
+++ srcs/ft_sort_balancer.c
@@ -6,7 +6,7 @@
 /*   By: kfalia-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/31 19:44:26 by kfalia-f          #+#    #+#             */
-/*   Updated: 2019/04/24 19:10:39 by kfalia-f         ###   ########.fr       */
+/*   Updated: 2019/04/24 19:14:55 by kfalia-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	ft_output(t_data *head, t_flags fl, int to_free, char *path)
 				ft_print(head, fl);
 			else
 		        ft_print_simple(head, fl);
-			ft_free_list(head);
+			ft_free_list(&head, 0);
 			return ;
 		}
 	}
