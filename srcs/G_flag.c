4da750d068c0f7ba3cfcb7e875ca8215518ec04a
Korbin Parker <koparker@vo-j4.21-school.ru>
2019-04-26 16:54:42 +0300

    G_flag leaks because of strjoin fix

--- srcs/G_flag.c
+++ srcs/G_flag.c
@@ -6,7 +6,7 @@
 /*   By: koparker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 14:48:00 by koparker          #+#    #+#             */
-/*   Updated: 2019/04/26 16:35:19 by koparker         ###   ########.fr       */
+/*   Updated: 2019/04/26 16:53:58 by koparker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,21 @@
 
 size_t  ft_sticky_file(char *perm, char *name)
 {
+	char	*tmp;
+
+	tmp = NULL;
 	if (*perm == '-')
 	{
 		if (ft_strrchr(perm, 't') || ft_strchr(perm, 'x'))
 		{
 			if (ft_strnchr(perm, 's', 3) || ft_strnchr(perm, 'S', 3))
-				ft_putstr(ft_strjoin(EXE_SETUID_COLOR, name));
+				tmp = ft_strjoin(EXE_SETUID_COLOR, name);
 			else if (ft_strnchr(perm, 's', 6) || ft_strnchr(perm, 'S', 6))
-				ft_putstr(ft_strjoin(EXE_SETGID_COLOR, name));
+				tmp = ft_strjoin(EXE_SETGID_COLOR, name);
 			else
-				ft_putstr(ft_strjoin(EXE_COLOR, name));
+				tmp = ft_strjoin(EXE_COLOR, name);
+			ft_putstr(tmp);
+			ft_strdel(&tmp);
 			return (1);
 		}
 	}
@@ -32,12 +37,17 @@ size_t  ft_sticky_file(char *perm, char *name)
 
 size_t  ft_sticky_dir(char *perm, char *name)
 {
+	char	*tmp;
+
+	tmp = NULL;
 	if (ft_strnchr(perm, 'w', 8) && *perm == 'd')
 	{
 		if (ft_strrchr(perm, 't') || ft_strrchr(perm, 'T'))
-			ft_putstr(ft_strjoin(STICKY_BIT_COLOR, name));
+			tmp = ft_strjoin(STICKY_BIT_COLOR, name);
 		else
-			ft_putstr(ft_strjoin(NO_STICKY_BIT_COLOR, name));
+			tmp = ft_strjoin(NO_STICKY_BIT_COLOR, name);
+		ft_putstr(tmp);
+		ft_strdel(&tmp);
 		return (1);
 	}
 	return (0);
@@ -64,21 +74,29 @@ void    ft_set_permissions(t_data **head, char *path)
 
 void    ft_colorized_output(char *perm, char *name)
 {
+	char	*tmp;
+
+	tmp = NULL;
 	if (ft_sticky_file(perm, name))
 		ft_putstr(RESET);
 	else if (ft_sticky_dir(perm, name))
 		ft_putstr(RESET);
 	else if (*perm == 'd')
-		ft_putstr(ft_strjoin(DIR_COLOR, name));
+		tmp = ft_strjoin(DIR_COLOR, name);
 	else if (*perm == 'b')
-		ft_putstr(ft_strjoin(BLOCK_COLOR, name));
+		tmp = ft_strjoin(BLOCK_COLOR, name);
 	else if (*perm == 'c')
-		ft_putstr(ft_strjoin(CHAR_COLOR, name));
+		tmp = ft_strjoin(CHAR_COLOR, name);
 	else if (*perm == 'l')
-		ft_putstr(ft_strjoin(SIMLNK_BASE_COLOR, name));
+		tmp = ft_strjoin(SIMLNK_BASE_COLOR, name);
 	else if (ft_strchr(perm, 'x'))
-		ft_putstr(ft_strjoin(EXE_COLOR, name));
+		tmp = ft_strjoin(EXE_COLOR, name);
 	else
 		ft_putstr(name);
+	if (tmp)
+	{
+		ft_putstr(tmp);
+		ft_strdel(&tmp);
+	}
 	ft_putstr(RESET);
 }
