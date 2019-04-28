c88fe17a7a9cf0dad55ad4ddac8b73ca8c46706b
Kassandra Falia flowers <kfalia-f@vo-i4.21-school.ru>
2019-04-28 17:44:01 +0300

    norm path_to_file

--- libft/ft_ls_path_to_file.c
+++ libft/ft_ls_path_to_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ls_path_to_file.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kfalia-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kfalia-f <kfalia-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 17:53:39 by kfalia-f          #+#    #+#             */
-/*   Updated: 2019/04/28 13:23:07 by koparker         ###   ########.fr       */
+/*   Updated: 2019/04/28 17:40:24 by kfalia-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,17 @@ char	*ft_path(char *path, int len)
 	return (res);
 }
 
-char	*ft_ls_path_to_file(char *path, int flag)
+char	*ft_pt(char *path, int flag, int len, int i)
 {
-	int		i;
-	int		len;
 	int		k;
 	char	*file;
 
-	i = 0;
-	len = 0;
-	while (path[i])
+	while (path[++i])
 	{
 		if (path[i] != '/')
-		{
-			i++;
 			continue ;
-		}
 		if (path[i] == '/')
 			len = i;
-		i++;
 	}
 	if (len == 0 && flag && path[0] != '/')
 		return (path);
@@ -65,4 +57,9 @@ char	*ft_ls_path_to_file(char *path, int flag)
 		return (file);
 	free(file);
 	return (ft_path(path, len));
+}
+
+char	*ft_ls_path_to_file(char *path, int flag)
+{
+	return (ft_pt(path, flag, 0, -1));
 }
